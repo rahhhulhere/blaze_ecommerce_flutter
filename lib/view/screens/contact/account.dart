8bc2b64c7abc52b459c17264ef41fba8981c57f1
Rahul Tiwari <tiwari.rahul@mail.com>
2023-02-27 21:00:26 +0530

    Bottom Drawer Part 1 | Account Screen

--- lib/view/screens/contact/account.dart
+++ lib/view/screens/contact/account.dart
@@ -42,73 +42,276 @@ class _AccountScreenState extends State<AccountScreen> {
         titleTextStyle: const TextStyle(),
       ),
       drawer: const DrawerWidget(),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: width * 0.3,
-                height: height * 0.3,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.grey,
+      body: Container(
+        height: height,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: width * 0.3,
+                  height: height * 0.3,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppText(
+                        Title: "Rahul Tiwari",
+                        Size: 16,
+                        Color: PrimaryColor,
+                      ),
+                      AppText(
+                        Title: "tiwarirahul2508@gmail.com",
+                        Size: 12,
+                        Color: PrimaryColor,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                height: height * 0.4,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    AppText(
-                      Title: "Rahul Tiwari",
-                      Size: 16,
-                      Color: PrimaryColor,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.edit,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Edit Profile",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
                     ),
-                    AppText(
-                      Title: "tiwarirahul2508@gmail.com",
-                      Size: 12,
-                      Color: PrimaryColor,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.pin_drop_rounded,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Shipping Address",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.watch_later,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Order History",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.track_changes,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Track Order",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.credit_card,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Cards",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: height * 0.045,
+                          width: width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFFFA1A1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child: const Icon(
+                            Icons.logout,
+                            color: Color(0xFFFA5C5C),
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.7,
+                          alignment: Alignment.centerLeft,
+                          child: AppText(
+                            Title: "Log Out",
+                            Color: Colors.black,
+                            Size: 15,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 18,
+                        )
+                      ],
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    height: height * 0.045,
-                    width: width * 0.1,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFFFFA1A1),
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                    ),
-                    child: const Icon(
-                      Icons.edit,
-                      color: Color(0xFFFA5C5C),
-                      size: 20,
-                    ),
-                  ),
-                  AppText(
-                    Title: "Edit Profile",
-                    Color: Colors.black,
-                    Size: 15,
-                  ),
-                  const Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.black,
-                    size: 18,
-                  )
-                ],
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
+      bottomNavigationBar: myNavBar(context),
     );
   }
+}
+
+Container myNavBar(BuildContext context) {
+  return Container(
+    height: 60,
+    decoration: BoxDecoration(
+      border: Border.all(
+        color: RedColor,
+        width: 1.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(40),
+        topRight: Radius.circular(40),
+      ),
+    ),
+    child: Row(
+      children: [
+        IconButton(
+            onPressed: () {
+              setState() {
+                // pageIndec = 0;
+              }
+            },
+            icon: Icon(Icons.confirmation_num_sharp))
+      ],
+    ),
+  );
 }
